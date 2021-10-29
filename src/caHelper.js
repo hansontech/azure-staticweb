b1e6f29142a57acd0619bd0246feec74d6687d68
hansontech <denniskung68@hotmail.com>
2021-10-29 21:34:31 +1000

    auth test

--- src/caHelper.js
+++ src/caHelper.js
@@ -1,28 +1,28 @@
 
 
 export default {
-    async getUserInfo() {
-      try {
-	const response = await fetch('/.auth/me');
-	const payload = await response.json();
-	const { clientPrincipal } = payload;
-	return clientPrincipal;
-      } catch (e) {
-	return 'not available'
-      }
-    },
-    async userSignIn() {
-	try {
-	  await fetch('/.auth/login/aad')
-	} catch (e) {
-	  console.log(e)
-	}
-    },
-    async userSignOut() {
-	try {
-	  await fetch('/.auth/logout')
-	} catch (e) {
-	  console.log(e)
-	}
+  async getUserInfo() {
+    try {
+     const response = await fetch('/.auth/me');
+     const payload = await response.json();
+     const { clientPrincipal } = payload;
+     return clientPrincipal;
+    } catch (e) {
+     return 'not available'
     }
+  },
+  async userSignIn() {
+   try {
+     await fetch('/.auth/login/aad')
+   } catch (e) {
+     console.log(e)
+   }
+  },
+  async userSignOut() {
+   try {
+    await fetch('/.auth/logout')
+   } catch (e) {
+    console.log(e)
+   }
+  }
 }
